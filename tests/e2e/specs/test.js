e17a07450a83a6629150eb7e0ebc5ec39028b42b
Josh Newton <josh-newton@hotmail.co.uk>
2020-02-22 10:15:54 +0000

    Fix e2e tests where classes or html elements had changed

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -14,26 +14,28 @@ describe('Todo List', () => {
   });
 
   it('Removes an item from the list', () => {
-    // 3 items, with table header row and hidden add item row
-    cy.get('.list table').find('tr').should('have.length', 5)
-    cy.get('.remove i').last().click()
+    // 3 items, with table header row
     cy.get('.list table').find('tr').should('have.length', 4)
+    cy.get('.remove-button i').last().click()
+    // Now 2 items, with table header row
+    cy.get('.list table').find('tr').should('have.length', 3)
   });
 
   it('Clears all items from the list', () => {
-    cy.get('.list table').find('tr').should('have.length', 5)
-    cy.get('.clear-all i').last().click()
-    // 2 allows for the table header and hidden add item row
+    cy.get('.list table').find('tr').should('have.length', 4)
+    cy.get('.clear-all-button i').last().click()
+    // 2 allows for the table header, and row saying all
+    // todos are complete
     cy.get('.list table').find('tr').should('have.length', 2)
   });
 
   it('Orders by priority', () => {
-    cy.get('.priority span').last().then((span) => {
+    cy.get('.item-priority span').last().then((span) => {
       const text = span.text()
       expect(text).to.equal('Meh')
     })
     cy.get('.priority-toggle').click()
-    cy.get('.priority span').last().then((span) => {
+    cy.get('.item-priority span').last().then((span) => {
       const text = span.text()
       expect(text).to.equal('Life changing')
     })
